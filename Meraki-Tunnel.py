83f69f2ed2d960c050ce9b1a3fd0c686a8810866
MitchellGulledge <61255163+MitchellGulledge@users.noreply.github.com>
2020-07-21 18:56:40 -0700

    Update Meraki-Tunnel.py
    Updating code to throw functions on top and tons of comments revisions etc.

--- Meraki-Tunnel.py
+++ Meraki-Tunnel.py
@@ -1,158 +1,183 @@
 import requests, json, time
 import meraki
-from io import BytesIO
-from operator import itemgetter
-from passwordgenerator import pwgenerator
-import logging
 import re
-import urllib.request
-import os
-import datetime as dt
 from datetime import datetime, timedelta
 import ast
 import sys
+import base64
 
-# This code is for automation of tunnels between MX Security Appliances and Umbrella SIG
+# Author: Mitchell Gulledge
 
-# Meraki credentials are placed below
-meraki_config = {
-	'api_key': "",
-	'orgName': ""
-}
+# class that contains all Meraki necessary config
+class MerakiConfig:
+    api_key = ''
+    org_name = ''
+    tag_prefix = 'SIG-'
+    org_id = None
+
+# class that contains all Umbrella necessary config
+class UmbrellaConfig:
+    # this is obtained from the api keys tab and specifically the umbrella management keys
+    key = ''
+    secret = ''
+    # Command -  echo -n 'secret:key' | base64 in terminal
+    base64_value = key + ':' + secret
+    message_bytes = base64_value.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
+    tunnel_url = "https://management.api.umbrella.com/v1/organizations/2506818/tunnels"
+    # creating header to authenticate API requests to Umbrella
+    headers = {'Authorization': 'Basic ' + base64_message}
 
 # writing function to obtain org ID via linking ORG name
-mdashboard = meraki.DashboardAPI(meraki_config['api_key'])
+mdashboard = meraki.DashboardAPI(MerakiConfig.api_key)
 result_org_id = mdashboard.organizations.getOrganizations()
 for x in result_org_id:
-    if x['name'] == meraki_config['orgName']:
-        meraki_config['org_id'] = x['id']
+    if x['name'] == MerakiConfig.org_name:
+        MerakiConfig.org_id = x['id']
 
-# branch subnets is a variable to display local branch site info
-branchsubnets = []
+# defining function that creates dictionary of IPsec config from Umbrella config
+def get_meraki_ipsec_config(name, local_id, public_ip, secret, network_tags) -> dict:
+    ipsec_config = {
+        "name": name,
+        "ikeVersion": "2",
+        "myUserFqdn": local_id,
+        "publicIp": public_ip,
+        "privateSubnets": "0.0.0.0/0",
+        "secret": secret,
+        "ipsecPolicies": {
+            "ikeCipherAlgo": ["aes256"],
+            "ikeAuthAlgo": ["sha256"],
+            "ikeDiffieHellmanGroup": ["group14"],
+            "ikeLifetime": 28800,
+            "childCipherAlgo": ["aes256"],
+            "childAuthAlgo": ["sha256"],
+            "childPfsGroup": ["group14"],
+            "childLifetime": 3600
+        },
+        "networkTags": network_tags
+    }
+    return ipsec_config
+
 # variable with new and existing s2s VPN config
 merakivpns = []
 
-# performing initial get to obtain all Meraki existing VPN info to add to merakivpns list above
+# performing initial get to obtain all Meraki existing VPN info 
 originalvpn = mdashboard.organizations.getOrganizationThirdPartyVPNPeers(
-    meraki_config['org_id']
+    MerakiConfig.org_id
 )
+# appending original vpn variable to the placeholder list merakivpns that was created above
 merakivpns.append(originalvpn)
 
 # Meraki call to obtain Network information
-tagsnetwork = mdashboard.networks.getOrganizationNetworks(meraki_config['org_id'])
+tags_network = mdashboard.networks.getOrganizationNetworks(MerakiConfig.org_id)
 
 # loop that iterates through the variable tagsnetwork and matches networks with SIG- in the tag
-for i in tagsnetwork:
-    if i['tags'] is None or i['name'] == 'Tag-Placeholder':
+for meraki_networks in tags_network:
+    if meraki_networks['tags'] is None:
+        print(meraki_networks['name'])
         pass
-    # searches for any network that has a tag beginning with SIG-, this is since each meraki network will need a unique identifier
-    elif "SIG-" in i['tags']: 
-        network_info = i['id'] # need network ID in order to obtain device/serial information
-        netname = i['name'] # network name used to label Meraki VPN and Umbrella config
-        nettag = i['tags']  # obtaining all tags for network as this will be placed in VPN config
-        devices = mdashboard.devices.getNetworkDevices(network_info) # call to get device info
-        xdevices = devices[0] # just parsing brackets
-        up = xdevices['serial'] # serial number to later obtain the uplink information for the appliance
-        firmwareversion = xdevices['firmware'] # now we obtained the firmware version, need to still add the validation portion
-        firmwarecompliance = str(firmwareversion).startswith("wired-15") # validation to say True False if MX appliance is on 15 firmware
+    # searches for any network that has a tag beginning with SIG-
+    elif "SIG-" in meraki_networks['tags']: 
+        # obtaining network ID in order to obtain device information
+        network_info = meraki_networks['id'] 
+        # network name used to label Meraki VPN and Umbrella ipsec config
+        netname = meraki_networks['name'] 
+        # obtaining all tags for network as this will be placed in VPN config
+        nettag = meraki_networks['tags']  
+        # call to get device info
+        devices = mdashboard.devices.getNetworkDevices(network_info)[0]
+        # validating firmware to ensure device is on 15
+        firmwareversion = devices['firmware'] 
+        # validation to say True False if MX appliance is on 15 firmware
+        firmwarecompliance = str(firmwareversion).startswith("wired-15") 
         if firmwarecompliance == True:
             print("firmware is compliant")
         else:
             break # if box isnt firmware compliant we break from the loop
-        modelnumber = xdevices['model']
 
         primary_vpn_tunnel_ip = '' # variable for umbrella public IP
         # detecting region to determine umbrella public IP addresses to place in IPsec config
-        if "SIG-PA-" in i['tags']: # US West Region
+        if "SIG-PA-" in meraki_networks['tags']: # US West Region
             # primary tunnel will be built to the PA PoP
             primary_vpn_tunnel_ip = '146.112.67.8'
-        elif "SIG-LA-" in i['tags']: # US West Region
+        elif "SIG-LA-" in meraki_networks['tags']: # US West Region
             # primary tunnel will be built to the LA PoP
             primary_vpn_tunnel_ip = '146.112.66.8'
-        elif "SIG-NY-" in i['tags']: # East US Region
+        elif "SIG-NY-" in meraki_networks['tags']: # East US Region
             # primary tunnel will be built to the NY PoP
             primary_vpn_tunnel_ip = '146.112.83.8'
-        elif "SIG-VA-" in i['tags']: # East US Region
+        elif "SIG-VA-" in meraki_networks['tags']: # East US Region
             # primary tunnel will be built to the VA PoP
             primary_vpn_tunnel_ip = '146.112.82.8'
-        elif "SIG-UK-" in i['tags']: # UK Region
+        elif "SIG-UK-" in meraki_networks['tags']: # UK Region
             # primary tunnel will be built to the UK PoP
             primary_vpn_tunnel_ip = '146.112.97.8'
-        elif "SIG-DE-" in i['tags']: # Frankfurt Region
+        elif "SIG-DE-" in meraki_networks['tags']: # Frankfurt Region
             # primary tunnel will be built to the Frankfurt PoP
             primary_vpn_tunnel_ip = '146.112.96.8'
-        elif "SIG-SG-" in i['tags']: # Singapore Region
+        elif "SIG-SG-" in meraki_networks['tags']: # Singapore Region
             # primary tunnel will be built to the SG PoP
             primary_vpn_tunnel_ip = '146.112.113.8'
-        elif "SIG-JP-" in i['tags']: # Asia Region
+        elif "SIG-JP-" in meraki_networks['tags']: # Asia Region
             # primary tunnel will be built to the Tokyo PoP
             primary_vpn_tunnel_ip = '146.112.112.8'
-        elif "SIG-SYD-" in i['tags']: # Aus Region
+        elif "SIG-SYD-" in meraki_networks['tags']: # Aus Region
             # primary tunnel will be built to the Sydney PoP
             primary_vpn_tunnel_ip = '146.112.118.8'
-        elif "SIG-ME-" in i['tags']: # Aus Region
+        elif "SIG-ME-" in meraki_networks['tags']: # Aus Region
             # primary tunnel will be built to the Melbourne PoP
             primary_vpn_tunnel_ip = '146.112.119.8'
         
-        # Set basics to make an API call to Umbrella for tunnel creation
-        url = '<a href="https://management.api.umbrella.com/v1/organizations/2506818/tunnels">https://management.api.umbrella.com/v1/organizations/2506818/tunnels</a>'
+        # creating umbrella ipsec config to be the data in the post, netname variable is tunnel name
         umbrella_tunnel_name = {"name": netname}
-        data = json.dumps(umbrella_tunnel_name)
-        # Executed following command to yield base64 of API Key:Secret. 
-        # Command -  echo -n 'secret:key' | base64
-        headers = {'Authorization': 'Basic <base64 value>'}
+        umbrella_tunnel_data = json.dumps(umbrella_tunnel_name)
 
-        tunnel_already_made = False # placeholder variable that will get flipped true if Umbrella API indicates that the tunnel name already exists
+        # variable flipped true if Umbrella API indicates that the tunnel name already exists
+        tunnel_already_made = False 
 
-        # Send HTTP req to Umbrella 
-        req = requests.post('https://management.api.umbrella.com/v1/organizations/<Add umbrella org ID here>/tunnels', headers=headers, data=data)
-        print(req.reason)
-        if req.reason == 'Conflict': # if we obtain a reason code of Conflict meaning the tunnel is already made we will continue
+        # Send HTTP req to Umbrella to create tunnel
+        req = requests.post(UmbrellaConfig.tunnel_url, headers=UmbrellaConfig.headers, \
+            data=umbrella_tunnel_data)
+         # if we obtain a reason code of Conflict meaning the tunnel is already made we will continue
+        if req.reason == 'Conflict':
             tunnel_already_made = True
             print("tunnel already made")
         else:
             tunnelRsp = json.loads(req.text)
             # Access tunnel ID
             tunnelId = tunnelRsp["id"]
-            print(tunnelId)
 
             # Access PSK id:key
             client = tunnelRsp["client"]
-            print(client["deviceType"])
 
-            tunnelPSKFqdn = client["authentication"]["parameters"]["id"] # parsing the local id/fqdn for the meraki vpn config here
-            tunnelPSKSecret = client["authentication"]["parameters"]["secret"] # parsing the pre shared key for the meraki vpn config here
-
-        # need to do a post to umbrella with the netname variable as the tunnel name
+            # parsing the local id/fqdn for the meraki vpn config here
+            tunnelPSKFqdn = client["authentication"]["parameters"]["id"] 
+            # parsing the pre shared key for the meraki vpn config here
+            tunnelPSKSecret = client["authentication"]["parameters"]["secret"] 
 
         # below parses the for the specific network tag on the network that correlates with SIG-
         specifictag = re.findall(r'[S]+[I]+[G]+[-].*', str(nettag))
         specifictag1 = re.findall(r'^([\S]+)', str(specifictag[0]))
         print(specifictag1[0])
 
-        # need to start building a dictionary (might be string for now) to append to the array of meraki vpns
         # sample IPsec template config that is later replaced with corresponding Azure variables (PSK pub IP, lan IP etc)
 
         if tunnel_already_made == False:
-            primary_vpn_tunnel_template = '{"name":"placeholder","publicIp":"192.0.0.0","privateSubnets":["0.0.0.0/0"],"secret":"meraki123", "ipsecPolicies":{"ikeCipherAlgo":["aes256"],"ikeAuthAlgo":["sha1"],"ikeDiffieHellmanGroup":["group2"],"ikeLifetime":28800,"childCipherAlgo":["aes256"],"childAuthAlgo":["sha1"],"childPfsGroup":["disabled"],"childLifetime":28800},"networkTags":["west"], "myUserFqdn":"mitch@umbrella.com"}'
-            primary_vpn_tunnel_tag = primary_vpn_tunnel_template.replace('west', specifictag1[0]) # applies specific tag from org overview page to ipsec config
-            primary_vpn_ip = primary_vpn_tunnel_tag.replace('192.0.0.0', primary_vpn_tunnel_ip)   # change variable to primary_vpn_tunnel_ip value
-            primary_vpn_tunnel_name = primary_vpn_ip.replace('placeholder' , netname) # replaces placeholder value with dashboard network name
-            add_vpn_psk = primary_vpn_tunnel_name.replace('meraki123', tunnelPSKSecret) # replace with pre shared key variable generated above
-            # need to add fqdn section
-            print(add_vpn_psk)
-            add_vpn_fqdn = add_vpn_psk.replace('mitch@umbrella.com',tunnelPSKFqdn)
-            print(add_vpn_fqdn)
+            # Build meraki config for IPsec configuration (using get_meraki_ipsec_config function)
+            primary_vpn_tunnel_template = get_meraki_ipsec_config(netname, tunnelPSKFqdn, \
+                primary_vpn_tunnel_ip, tunnelPSKSecret, specific_tag1[0])
+          
             newmerakivpns = merakivpns[0]
             # appending newly created tunnel config to original VPN list
-            newmerakivpns.append(json.loads(add_vpn_fqdn)) # appending new vpn config with original vpn config
-            #newmerakivpns.append(json.loads(secondary_vpn_tunnel_template)) # appending backup tunnel config to vpn list
+            newmerakivpns.append(json.loads(primary_vpn_tunnel_template)) # appending new vpn config with original vpn config
             print(newmerakivpns)
 
-        else:
+        else: 
             print("tunnel already created")
 
 # final call to update Meraki VPN config
 updatemvpn = mdashboard.organizations.updateOrganizationThirdPartyVPNPeers(
-    meraki_config['org_id'], merakivpns[0]
+    MerakiConfig.org_id, merakivpns[0]
     )
+    
