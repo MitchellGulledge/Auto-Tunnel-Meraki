de7c3365dab8dfec893d7aa5de52e232864f894d
MitchellGulledge <61255163+MitchellGulledge@users.noreply.github.com>
2020-10-27 18:43:30 -0400

    Update Meraki-Tunnel.py
    Still need to clean up the tag function as the indexing now is pretty ugly

--- Meraki-Tunnel.py
+++ Meraki-Tunnel.py
@@ -10,26 +10,56 @@ from ip2geotools.databases.noncommercial import DbIpCity
 
 # class that contains all Meraki necessary config
 class MerakiConfig:
-    api_key = '' 
+    api_key = ''
     org_name = ''
     tag_prefix = 'SIG-'
     org_id = None
+    sdk_auth = meraki.DashboardAPI(api_key)
+
+    # writing function to obtain org ID via linking ORG name
+    result_org_id = sdk_auth.organizations.getOrganizations()
+    for x in result_org_id:
+        if x['name'] == org_name:
+            org_id = x['id']
+
+    # creating original list of Meraki VPNs to later append to
+    meraki_vpn_list = []
+
+    # obtaining original list of Meraki third party VPNs
+    original_vpn = sdk_auth.appliance.getOrganizationApplianceVpnThirdPartyVPNPeers(
+    org_id) 
+
+    meraki_vpn_list = original_vpn['peers']
+
+    # Meraki call to obtain Network information
+    tags_network = sdk_auth.organizations.getOrganizationNetworks(org_id)
+
+    # filtering None types from the list using filter
+    res_tags_network = list(filter(None, tags_network)) 
 
 # class that contains all Umbrella necessary config
 class UmbrellaConfig:
     # this is obtained from the api keys tab and specifically the umbrella management keys
     key = ''
     secret = ''
-    org_id = '\'
+    org_id = ''
+
     # Command -  echo -n 'secret:key' | base64 in terminal
     base64_value = key + ':' + secret
     message_bytes = base64_value.encode('ascii')
     base64_bytes = base64.b64encode(message_bytes)
     base64_message = base64_bytes.decode('ascii')
+
     # url for network tunnels in umbrella dashboard
     tunnel_url = "https://management.api.umbrella.com/v1/organizations/"+org_id+"/tunnels"
+
     # url for listing umbrella DCs
     dc_url = 'https://management.api.umbrella.com/v1/service/tunnel/datacenters'
+
+    # delete umbrella tunnel url 
+    delUrl = 'https://management.api.umbrella.com/v1/organizations/'+ \
+        org_id+'/tunnels/'
+
     # creating header to authenticate API requests to Umbrella
     headers = {'Authorization': 'Basic ' + base64_message}
 
@@ -38,14 +68,11 @@ def strip_meraki_network_tags(meraki_network_tag):
     # below parses the for the specific network tag on the network that correlates with SIG-
     meraki_tag_strip_part1 = re.findall(r'[S]+[I]+[G]+[-].*', str(meraki_network_tag))
     meraki_tag_strip_part2 = re.findall(r'^([\S]+)', str(meraki_tag_strip_part1[0]))
-    return meraki_tag_strip_part2[0]
-
-# writing function to obtain org ID via linking ORG name
-mdashboard = meraki.DashboardAPI(MerakiConfig.api_key)
-result_org_id = mdashboard.organizations.getOrganizations()
-for x in result_org_id:
-    if x['name'] == MerakiConfig.org_name:
-        MerakiConfig.org_id = x['id']
+    print("look below")
+    new_string = str(meraki_tag_strip_part2[0])
+    new_string = new_string[0:-2]
+    print(new_string)
+    return new_string
 
 # defining function that creates dictionary of IPsec config from Umbrella config
 def get_meraki_ipsec_config(name, public_ip, secret, network_tags, local_id) -> dict:
@@ -102,7 +129,7 @@ def get_dc_ip(networkId):
     mx_branch_ip = ''
 
     # obtaining branch MXs public IP w/ org wide network devices call
-    list_of_device_statuses = mdashboard.organizations.getOrganizationDeviceStatuses(
+    list_of_device_statuses = MerakiConfig.sdk_auth.organizations.getOrganizationDevicesStatuses(
         MerakiConfig.org_id)
 
     for device in list_of_device_statuses:
@@ -123,14 +150,13 @@ def get_dc_ip(networkId):
 
     # request to obtain list of DCs
     get_dc_req = requests.get(UmbrellaConfig.dc_url, headers=UmbrellaConfig.headers)
-
+    print(get_dc_req)
     # if response is successful begin building variables to feed into haversine formula
     if get_dc_req.status_code == 200:
         for datacenters in get_dc_req.json()['continents']:
             for umb_datacenter in datacenters['cities']:
                 if not umb_datacenter['range'].split('/')[0][-1] == '8':
                     continue
-                #print("Checking " + umb_datacenter['range'])
                 # umbrella dc latitude
                 lat2 = umb_datacenter['latitude']
                 # umbrella dc latitude
@@ -149,7 +175,6 @@ def get_dc_ip(networkId):
 
         return primary_vpn_tunnel_ip
 
-
 def delete_umbrella_tunnel(vpn_tunnel_name):
     # fetching list of umbrella tunnel config
     get_req = requests.get(UmbrellaConfig.tunnel_url, headers=UmbrellaConfig.headers)
@@ -163,12 +188,11 @@ def delete_umbrella_tunnel(vpn_tunnel_name):
             # parsing umbrella tunnel config for id
             umb_tunnel_id = tunnel["id"]
 
-            # umbrella URL class
-            delUrl = 'https://management.api.umbrella.com/v1/organizations/'+ \
-                UmbrellaConfig.org_id+'/tunnels/' + str(umb_tunnel_id)
+            # crafting tunnel specific url to delete ipsec config in umbrella dashboard
+            del_tunnel_url = str(UmbrellaConfig.delUrl) + str(umb_tunnel_id)
             
             # deleting Umbrella tunnel
-            delReq = requests.delete(delUrl, headers=UmbrellaConfig.headers)
+            delReq = requests.delete(del_tunnel_url, headers=UmbrellaConfig.headers)
             print(delReq.reason)
 
             if delReq.reason == 200:
@@ -177,14 +201,14 @@ def delete_umbrella_tunnel(vpn_tunnel_name):
 
 # function to update Meraki VPN config
 def update_meraki_vpn(vpn_list):
-    updatemvpn = mdashboard.organizations.updateOrganizationThirdPartyVPNPeers(
+    updatemvpn = MerakiConfig.sdk_auth.appliance.updateOrganizationApplianceVpnThirdPartyVPNPeers(
     MerakiConfig.org_id, vpn_list
     )
 
 # function to validate that MX is on version 15 or greater
 def validate_mx_firmware(branch_node):
     # call to get device info
-    devices = mdashboard.devices.getNetworkDevices(branch_node)
+    devices = MerakiConfig.sdk_auth.networks.getNetworkDevices(branch_node)
     print(devices)
     # validating firmware to ensure device is on 15
     firmwareversion = devices[0]['firmware'] 
@@ -217,33 +241,10 @@ def create_umbrella_tunnel(tunnel_name):
     
     return tunnelPSKFqdn, tunnelPSKSecret
 
-# this function performs initial get to obtain all Meraki existing VPN info 
-def get_meraki_ipsec_tunnels():
-    originalvpn = mdashboard.organizations.getOrganizationThirdPartyVPNPeers(
-        MerakiConfig.org_id)  
-    return originalvpn     
-
-# variable with new and existing s2s VPN config
-merakivpns = []
-
-# performing initial get to obtain all Meraki existing VPN info 
-original_meraki_tunnels = get_meraki_ipsec_tunnels()
-print(original_meraki_tunnels)
-
-# appending original vpn variable to the placeholder list merakivpns that was created above
-merakivpns.append(original_meraki_tunnels)
-merakivpns = merakivpns[0]
-
-# Meraki call to obtain Network information
-tags_network = mdashboard.networks.getOrganizationNetworks(MerakiConfig.org_id)
-
 # loop that iterates through the variable tagsnetwork and matches networks with SIG- in the tag
-for meraki_networks in tags_network:
-    # need to account for Nonetype datatype when iterating through loop
-    if meraki_networks['tags'] == None: 
-        pass
-    # searches for any network that has a tag beginning with SIG-
-    elif "SIG-" in meraki_networks['tags']: 
+for meraki_networks in MerakiConfig.res_tags_network:
+    if "SIG-" in str(meraki_networks['tags']): 
+        print(meraki_networks)
         # obtaining network ID in order to obtain device information
         network_info = meraki_networks['id'] 
         # network name used to label Meraki VPN and Umbrella ipsec config
@@ -290,7 +291,7 @@ for meraki_networks in tags_network:
         # if tunnel is built in umbrella already but not Meraki we need to detect and update config
         if tunnel_already_made == True:
             # iterating through original list of vpn tunnels from Meraki to match on name
-            for meraki_tunnel_name in merakivpns:
+            for meraki_tunnel_name in MerakiConfig.meraki_vpn_list:
                 if netname == meraki_tunnel_name['name']:
                     print("tunnel config in umbrella matches Meraki for " + netname)
                     # changing variable for being detected in umbrella and meraki config
@@ -326,9 +327,9 @@ for meraki_networks in tags_network:
             # creating variable to detect whether or not umbrella tunnel exists in Meraki config
             is_meraki_tunnel_updated = False
 
-            for vpn_peer_name in merakivpns:
+            for vpn_peer_name in MerakiConfig.meraki_vpn_list:
                 # iterating through list of existing meraki vpn peers validating if tunnel is created
-                if vpn_peer_name == merakivpns[0]['name']:
+                if vpn_peer_name == str(MerakiConfig.meraki_vpn_list):
                     print("peer detected in the config already updating PSK")
                     is_meraki_tunnel_updated = True
                     # updating psk for meraki vpn tunnel to umbrella
@@ -337,8 +338,8 @@ for meraki_networks in tags_network:
 
             if is_meraki_tunnel_updated == False:
                 # appending newly created tunnel config to original VPN list
-                merakivpns.append(primary_vpn_tunnel_template)          
+                MerakiConfig.meraki_vpn_list.append(primary_vpn_tunnel_template)      
 
 # final function performing update to Meraki VPN config
-print(merakivpns)
-update_meraki_vpn(merakivpns)
+print(MerakiConfig.meraki_vpn_list)
+update_meraki_vpn(MerakiConfig.meraki_vpn_list)
